97478b167e777280675bca0949707006db4bb753
slslash <sevenleftslash@proton.me>
2026-03-15 00:49:05 +0700

    whoops

--- src/util/json5pp.hpp
+++ src/util/json5pp.hpp
@@ -1,21 +1,3 @@
-/*
-** mkshot-z - Experimental OneShot (2016) engine reimplementation for modders.
-**
-** Copyright (C) 2026 Team Reverium <https://github.com/reverium>
-** Copyright (C) 2024 hat_kid <https://github.com/thehatkid> (ModShot-mkxp-z)
-** Copyright (C) 2013-2023 Amaryllis Kulla and mkxp-z contributors
-**
-** This program is free software: you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation, either version 3 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** GNU General Public License for more details.
-*/
-
 // https://github.com/kimushu/json5pp
 
 #pragma once
@@ -2185,5 +2167,3 @@ value::json_type value::stringify5(T... args) const
 }
 
 } /* namespace json5pp */
-
-#endif  /* _JSON5PP_HPP_ */
